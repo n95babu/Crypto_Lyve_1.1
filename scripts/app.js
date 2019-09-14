131ea0998439bb8ee1b54362962aca4140439350
Nazrul Babu <n95babu@gmail.com>
2019-09-14 12:07:39 -0400

    3rd Api call

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,8 @@
 /* eslint-disable no-console */
 const ul = document.querySelector('.assets');
 const ex = document.querySelector('.allExchanges');
+const jcap = document.querySelector('.tCap');
+
 const BASE_URL = 'https://api.coinranking.com/v1/public/coins';
 const EXCHNAGE_URL = 'https://api.coinranking.com/v1/public/exchanges';
 
@@ -22,23 +24,34 @@ const getCoins = async () => {
   coin.forEach((assets) => {
     const li = createNode('li');
     const span = createNode('span');
-    const h1 = createNode('h1')
+    const h3 = createNode('h3');
     const image = createNode('img');
     image.src = assets.iconUrl;
     span.innerHTML = ` $${parseFloat(assets.price).toFixed(2)} `;
-    h1.innerHTML = `${assets.name} `;
+    h3.innerHTML = `${assets.name} `;
     append(li, image);
     append(li, span);
-    append(li, h1);
+    append(li, h3);
     append(ul, li);
   });
 };
 getCoins();
 
+const MKCap = async () => {
+  const res = await axios.get(`${BASE_URL}`);
+  const cap = res.data.data.stats;
+  console.log(cap);
+  jcap.innerHTML = '';
+  const h4 = createNode('h4');
+  h4.innerHTML = `Market Cap: $${cap.totalMarketCap.toFixed(0)}`;
+  append(jcap, h4);
+};
+MKCap();
+
 const getExchanges = async () => {
   const res = await axios.get(`${EXCHNAGE_URL}`);
   const exchange = res.data.data.exchanges;
-  console.log(exchange);
+  // console.log(exchange);
   ex.innerHTML = '';
   exchange.forEach((exch) => {
     const li = createNode('li');
